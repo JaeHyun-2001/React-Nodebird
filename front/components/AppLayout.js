10893555347a28d98f91a13aa290c2fbf5be728e
JaeHyun <guswogus81@gmail.com>
2024-07-06 10:46:39 +0000

    React Nodebird : antd, styled-components install

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,15 +1,20 @@
 // componenets/AppLayout.js
 import React, { Children } from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link'
 
 const AppLayout = ({ children }) => {
-  return (
-    <div>
-      <div> 공통 메뉴</div>
-      {children}
-    </div>
-  );
-};
+    return (
+      <div>
+        <div>
+          <Link href="/" legacyBehavior><a>노드버드</a></Link>
+          <Link href="/profile" legacyBehavior><a>프로필</a></Link>
+          <Link href="/signup" legacyBehavior><a>로그인</a></Link>
+        </div>
+        {children}
+      </div>
+    );
+  };
 
 AppLayout.prototype = {
   children: PropTypes.node.isRequired,  // prop가 제공되지 않을 때 경고 문구를 출력함
